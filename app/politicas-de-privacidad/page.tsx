e25d9d7ba78eb1ac80b612035380de8d42aaae7d
felixvnolasco84 <felix@polygonag.com>
2024-08-13 16:50:36 -0600

    feat: Update image loading style and dimensions for improved responsiveness and performance

---
+++ app/politicas-de-privacidad/page.tsx
@@ -0,0 +1,20 @@
+import RichTextEditor from "@/components/TipTapOnlyContent";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Larena - Políticas de Privacidad",
+  description: "Conoce nuestras Políticas de Privacidad.",
+  alternates: {
+    canonical: "https://www.polygonag.com/politicas-de-privacidad",
+  },
+};
+
+let post: string =
+  "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, a.</p>";
+export default function Page() {
+  return (
+    <div className="container flex flex-col gap-y-7 px-4 py-12 text-justify lg:flex lg:gap-12">
+      <RichTextEditor content={post} />
+    </div>
+  );
+}
